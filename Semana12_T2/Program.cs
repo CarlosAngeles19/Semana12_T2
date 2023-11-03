88af7efc1e7e9c4bd3984550019f7094e7ee14f9
PC GAMER <PC GAMER@CHARLES>
2023-11-03 16:58:37 -0500

    Agregar archivos de proyecto.

---
+++ Semana12_T2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semana12_T2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int opcion;
+
+            do
+            {
+                Console.Clear();
+                opcion = Pantallas.PantallaPrincipal();
+
+                switch (opcion)
+                {
+                    case 1:
+                        opcion = Pantallas.IngresarNotas();
+                        break;
+                    case 2:
+                        opcion = Pantallas.NotaMasAlta();
+                        break;
+                    case 3:
+                        opcion = Pantallas.NotaMasBaja();
+                        break;
+                    case 4:
+                        opcion = Pantallas.ListaDeEstudiantes();
+                        break;
+                    
+                    case 0:
+                        break;
+                }
+            } while (opcion != 5);
+
+
+        }
+    }
+    }
